a5e6ca3bbfa622e020879288af386b4549a7cf20
Jorge Romero <romeroqj@gmail.com>
2014-10-24 13:19:55 -0600

    get_tag test passed

--- mixcloud.py
+++ mixcloud.py
@@ -1,18 +1,18 @@
 import requests
 
 class Mixcloud(object):
-    api_url = 'http://api.mixcloud.com/'
+    api_base = 'http://api.mixcloud.com/'
     
-    def _request(self, resource):
-        r  = requests.get(self.api_url + resource)
+    def request(self, resource):
+        r  = requests.get(self.api_base + resource)
         if r.status_code == 200:
             return r.json()
 
     def get_user(self, username):
-        return self._request(username)
+        return self.request(username)
 
     def get_tag(self, name):
-        pass
+        return self.request('tag/' + name)
 
     def get_artist(self, slug=None, name=None):
         pass
--- tests.py
+++ tests.py
@@ -3,10 +3,18 @@ import unittest
 import mixcloud
 
 class TestUsers(unittest.TestCase):
+
+    mc = mixcloud.Mixcloud()
+    
     def test_get_user(self):
-        mc = mixcloud.Mixcloud()
-        user = mc.get_user('romeroqj')
+        """ Test GET /<username> """
+        user = self.mc.get_user('romeroqj')
         self.assertEqual(user['username'], 'romeroqj')
+
+    def test_get_tag(self):
+        """ Test GET /tag/<tag> """
+        tag = self.mc.get_tag('house')
+        self.assertEqual(tag['name'], 'House')
 
 if __name__ == '__main__':
     unittest.main()
