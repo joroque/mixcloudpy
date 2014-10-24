245231d08d9d564daef4e8c0483ee081703a20bd
Jorge Romero <romeroqj@gmail.com>
2014-10-24 13:07:50 -0600

    get_user test passed

--- mixcloud.py
+++ mixcloud.py
@@ -1,5 +1,23 @@
 import requests
 
 class Mixcloud(object):
-    pass
+    api_url = 'http://api.mixcloud.com/'
+    
+    def _request(self, resource):
+        r  = requests.get(self.api_url + resource)
+        if r.status_code == 200:
+            return r.json()
+
+    def get_user(self, username):
+        return self._request(username)
+
+    def get_tag(self, name):
+        pass
+
+    def get_artist(self, slug=None, name=None):
+        pass
+
+    def get_cloudcast(self, username, slug=None, name=None):
+        pass
+
 
--- tests.py
+++ tests.py
@@ -3,6 +3,11 @@ import unittest
 import mixcloud
 
 class TestUsers(unittest.TestCase):
-    def test_user(self):
-        pass
-    
+    def test_get_user(self):
+        mc = mixcloud.Mixcloud()
+        user = mc.get_user('romeroqj')
+        self.assertEqual(user['username'], 'romeroqj')
+
+if __name__ == '__main__':
+    unittest.main()
+            
